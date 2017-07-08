39463a8065f304373af34dc90a755117c6ef349c
Johan Nordberg <its@johan-nordberg.com>
2017-07-08 22:10:53 +0200

    Add tests for transaction broadcasts

---
+++ test/broadcast.ts
@@ -0,0 +1,79 @@
+import 'mocha'
+import * as assert from 'assert'
+import {VError} from 'verror'
+
+import {Client, PrivateKey, signTransaction, CommentOperation, Transaction, utils} from './../src'
+
+import {testnet, getTestnetAccounts, randomString} from './common'
+
+describe('broadcast', function() {
+    this.slow(10 * 1000)
+    this.timeout(60 * 1000)
+
+    const {addr, chainId, addressPrefix} = testnet
+    const client = new Client(addr, {chainId, addressPrefix})
+
+    let acc1, acc2: {username: string, password: string}
+    before(async function() {
+        [acc1, acc2] = await getTestnetAccounts()
+    })
+
+    const postPermlink = `dsteem-test-${ randomString(5) }`
+
+    it('should broadcast', async function() {
+        const key = PrivateKey.fromLogin(acc1.username, acc1.password, 'posting')
+        const result = await client.broadcast.comment({
+            parent_author: '',
+            parent_permlink: 'parent',
+            author: acc1.username,
+            permlink: postPermlink,
+            title: 'I like turtles',
+            body: '🐢🐢🐢🐢🐢',
+            json_metadata: JSON.stringify({foo: 'bar'}),
+        }, key)
+        const block = await client.database.getBlock(result.block_num)
+        assert(block.transaction_ids.indexOf(result.id) !== -1)
+    })
+
+    it('should handle expired transactions', async function() {
+        const prevExpireTime = client.broadcast.expireTime
+        client.broadcast.expireTime = 0
+        const key = PrivateKey.fromLogin(acc2.username, acc2.password, 'posting')
+        try {
+            await client.broadcast.vote({
+                voter: acc2.username,
+                author: acc1.username,
+                permlink: postPermlink,
+                weight: 42,
+            }, key)
+            assert(false, 'should not be reached')
+        } catch (error) {
+            assert.equal(error.message, 'Transaction expired')
+            const info = VError.info(error)
+            assert.equal(info.trx_num, -1)
+        }
+        client.broadcast.expireTime = prevExpireTime
+    })
+
+    it('should handle concurrent broadcasts', async function() {
+        const key = PrivateKey.fromLogin(acc2.username, acc2.password, 'posting')
+        const commentPromise = client.broadcast.comment({
+            parent_author: acc1.username,
+            parent_permlink: postPermlink,
+            author: acc2.username,
+            permlink: `${ postPermlink }-comment-1`,
+            title: 'Comments has titles?',
+            body: 'ME ALSO!!',
+            json_metadata: '',
+        }, key)
+        const votePromise = client.broadcast.vote({
+            voter: acc2.username,
+            author: acc1.username,
+            permlink: postPermlink,
+            weight: 1337,
+        }, key)
+        const result = await Promise.all([commentPromise, votePromise])
+        assert(result.every((r) => r.expired === false))
+    })
+
+})
--- test/common.ts
+++ test/common.ts
@@ -1,6 +1,52 @@
 
+import * as fetch from 'node-fetch'
+import * as fs from 'fs'
+import {promisify} from 'util'
+import {randomBytes} from 'crypto'
+
+const readFile = promisify(fs.readFile)
+const writeFile = promisify(fs.writeFile)
+
+const NUM_TEST_ACCOUNTS = 2
+
 export const testnet = {
     addr: 'wss://testnet.steem.vc',
     chainId: '79276aea5d4877d9a25892eaa01b0adf019d3e5cb12a97478df3298ccdd01673',
     addressPrefix: 'STX',
+}
+
+export function randomString(length: number) {
+    return randomBytes(length*2)
+        .toString('base64')
+        .replace(/[^0-9a-z]+/gi, '')
+        .slice(0, length)
+        .toLowerCase()
+}
+
+export async function getTestnetAccounts(): Promise<{username: string, password: string}[]> {
+    try {
+        return JSON.parse(await readFile('.testnetrc'))
+    } catch (error) {
+        if (error.code !== 'ENOENT') {
+            throw error
+        }
+    }
+    console.warn('-- CREATING TESTNET ACCOUNTS --')
+    let rv: {username: string, password: string}[] = []
+    while (rv.length < NUM_TEST_ACCOUNTS) {
+        const password = randomString(32)
+        const username = `dsteem-${ randomString(9) }`
+        const response = await fetch('https://testnet.steem.vc/create', {
+            method: 'POST',
+            body: `username=${ username }&password=${ password }`,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        })
+        const text = await response.text()
+        if (response.status !== 200) {
+            throw new Error(`Unable to create user: ${ text }`)
+        }
+        rv.push({username, password})
+    }
+    await writeFile('.testnetrc', JSON.stringify(rv))
+    return rv
 }
