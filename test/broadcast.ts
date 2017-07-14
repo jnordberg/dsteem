8656294b64183e81403dc5e23c068446dd81f8cd
Johan Nordberg <its@johan-nordberg.com>
2017-07-14 18:42:47 +0200

    Add test for dropped connection in broadcast

--- test/broadcast.ts
+++ test/broadcast.ts
@@ -24,12 +24,12 @@ describe('broadcast', function() {
         const key = PrivateKey.fromLogin(acc1.username, acc1.password, 'posting')
         const result = await client.broadcast.comment({
             parent_author: '',
-            parent_permlink: 'parent',
+            parent_permlink: 'test',
             author: acc1.username,
             permlink: postPermlink,
             title: 'I like turtles',
-            body: '🐢🐢🐢🐢🐢',
-            json_metadata: JSON.stringify({foo: 'bar'}),
+            body: `🐢🐢🐢🐢🐢 ${ Math.random() * 42 }`,
+            json_metadata: JSON.stringify({foo: 'bar', tags: ['test']}),
         }, key)
         const block = await client.database.getBlock(result.block_num)
         assert(block.transaction_ids.indexOf(result.id) !== -1)
@@ -54,6 +54,30 @@ describe('broadcast', function() {
         }, key)
         const result = await Promise.all([commentPromise, votePromise])
         assert(result.every((r) => r.expired === false))
+    })
+
+    it('should handle dropped connection', async function() {
+        const key = PrivateKey.fromLogin(acc1.username, acc1.password, 'posting')
+        if (!client.isConnected()) {
+            await utils.waitForEvent(client, 'open')
+        }
+        const promise = client.broadcast.vote({
+            voter: acc1.username,
+            author: acc1.username,
+            permlink: postPermlink,
+            weight: 10000,
+        }, key)
+        const ac = client as any
+        try {
+            while (ac.broadcast.pendingCallbacks.size === 0) {
+                await utils.sleep(10)
+            }
+            client.disconnect()
+            await promise
+            assert(false, 'should not be reached')
+        } catch (error) {
+            assert.equal(error.message, 'Connection unexpectedly closed')
+        }
     })
 
 })
