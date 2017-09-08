a85e676515bc70ed98f0b9c26d913f4e95dc5f31
Johan Nordberg <its@johan-nordberg.com>
2017-09-08 09:50:31 +0200

    Polyfill browsers not supporting Number.isFinite

--- src/index-browser.ts
+++ src/index-browser.ts
@@ -35,6 +35,8 @@
 
 import 'core-js/es6/map'
 import 'core-js/es6/promise'
+import 'core-js/es6/symbol'
+import 'core-js/es6/number'
 import 'core-js/fn/array/from'
 import 'core-js/modules/es7.symbol.async-iterator'
 import 'regenerator-runtime/runtime'
