41e3d1bd4a99a16cccb463ed1038249aae2eb1ec
Johan Nordberg <its@johan-nordberg.com>
2017-09-08 13:48:04 +0200

    Add typed array polyfills

--- src/index-browser.ts
+++ src/index-browser.ts
@@ -40,9 +40,10 @@ if (global['navigator'] && /Edge/.test(global['navigator'].userAgent)) {
 }
 
 import 'core-js/es6/map'
+import 'core-js/es6/number'
 import 'core-js/es6/promise'
 import 'core-js/es6/symbol'
-import 'core-js/es6/number'
+import 'core-js/es6/typed'
 import 'core-js/fn/array/from'
 import 'core-js/modules/es7.symbol.async-iterator'
 import 'regenerator-runtime/runtime'
