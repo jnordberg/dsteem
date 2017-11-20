942a26f7df5555b0aef0b092a55b2348f0da9ed7
Johan Nordberg <its@johan-nordberg.com>
2017-11-20 20:43:18 +0100

    Break out protocol serializer tests
    Also added some more tests

--- test/serializers.ts
+++ test/serializers.ts
@@ -4,6 +4,12 @@ import * as ByteBuffer from 'bytebuffer'
 
 import {Types, Serializer, HexBuffer} from './../src/index-node'
 
+/*
+ Serializer tests in the format:
+ [{"name": "Type[::Subtype]", "values": [["expected output as hex string", <value>]]}]
+*/
+const serializerTests = require('./serializer-tests.json')
+
 function serialize(serializer: Serializer, data: any) {
     const buffer = new ByteBuffer(ByteBuffer.DEFAULT_CAPACITY, ByteBuffer.LITTLE_ENDIAN)
     serializer(buffer, data, {addressPrefix: 'STM'})
@@ -13,12 +19,21 @@ function serialize(serializer: Serializer, data: any) {
 
 describe('serializers', function() {
 
-    it('Array', function() {
-        const r1 = serialize(Types.Array(Types.String), ['foo', 'bar'])
-        assert.equal(r1, '0203666f6f03626172')
-        const r2 = serialize(Types.Array(Types.UInt16), [100, 200])
-        assert.equal(r2, '026400c800')
-    })
+    for (const test of serializerTests) {
+        it(test.name, () => {
+            let serializer: Serializer
+            if (test.name.indexOf('::') === -1) {
+                serializer = Types[test.name]
+            } else {
+                const [base, ...sub] = test.name.split('::').map((t) => Types[t])
+                serializer = base(...sub)
+            }
+            for (const [expected, value] of test.values) {
+                const actual = serialize(serializer, value)
+                assert.equal(actual, expected)
+            }
+        })
+    }
 
     it('Buffer', function() {
         const data = HexBuffer.from('026400c800')
@@ -33,45 +48,11 @@ describe('serializers', function() {
         })
     })
 
-    it('Date', function() {
-        const r1 = serialize(Types.Date, '2017-07-15T16:51:19')
-        assert.equal(r1, '07486a59')
-        const r2 = serialize(Types.Date, '2000-01-01T00:00:00')
-        assert.equal(r2, '80436d38')
-    })
-
-    it('Transaction', function() {
-        const tx: any = {
-            ref_block_num: 1234,
-            ref_block_prefix: 1122334455,
-            expiration: '2017-07-15T16:51:19',
-            extensions: [
-                'long-pants'
-            ],
-            operations: [
-                ['vote', {voter: 'foo', author: 'bar', permlink: 'baz', weight: 10000}]
-            ]
-        }
-        const r1 = serialize(Types.Transaction, tx)
-        assert.equal(r1, 'd204f776e54207486a59010003666f6f036261720362617a1027010a6c6f6e672d70616e7473')
-    })
-
     it('Void', function() {
         assert.throws(() => { serialize(Types.Void, null) })
     })
 
-    it('Optional', function() {
-        const r1 = serialize(Types.Optional(Types.Boolean), true)
-        const r2 = serialize(Types.Optional(Types.Boolean), undefined)
-        assert.equal(r1, '0101')
-        assert.equal(r2, '00')
-    })
-
-    it('Operation', function() {
-        const r1 = serialize(Types.Operation, ['transfer', {
-            from: 'foo', to: 'bar', amount: 1, memo: 'wedding present'
-        }])
-        assert.equal(r1, '0203666f6f03626172e80300000000000003535445454d00000f77656464696e672070726573656e74')
+    it('Invalid Operations', function() {
         assert.throws(() => { serialize(Types.Operation, ['transfer', {}]) })
         assert.throws(() => { serialize(Types.Operation, ['transfer', {from: 1}]) })
         assert.throws(() => { serialize(Types.Operation, ['transfer', 10]) })
