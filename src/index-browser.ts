7b34bffb747c1893f6caa90fe6961d474d6405d0
Johan Nordberg <its@johan-nordberg.com>
2018-11-30 15:00:17 +0100

    Fix regenerator runtime for babel 7
    Not sure exactly whats going on here, making it the first import solves the problem

--- src/index-browser.ts
+++ src/index-browser.ts
@@ -33,6 +33,8 @@
  * in the design, construction, operation or maintenance of any military facility.
  */
 
+import 'regenerator-runtime/runtime'
+
 // Microsoft is keeping to their long-held tradition of shipping broken
 // standards implementations, this forces Edge to use the polyfill insted.
 // tslint:disable-next-line:no-string-literal
@@ -46,7 +48,6 @@ import 'core-js/es6/promise'
 import 'core-js/es6/symbol'
 import 'core-js/fn/array/from'
 import 'core-js/modules/es7.symbol.async-iterator'
-import 'regenerator-runtime/runtime'
 import 'whatwg-fetch'
 
 export * from './index'
