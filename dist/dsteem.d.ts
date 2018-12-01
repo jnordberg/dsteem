b280bb20b0510042f49ef0cdf510fda5bb22e231
Johan Nordberg <its@johan-nordberg.com>
2018-12-01 01:27:31 +0100

    Version 0.11.2

--- dist/dsteem.d.ts
+++ dist/dsteem.d.ts
@@ -2578,13 +2578,13 @@ declare module 'dsteem/index-browser' {
 	 * You acknowledge that this software is not designed, licensed or intended for use
 	 * in the design, construction, operation or maintenance of any military facility.
 	 */
+	import 'regenerator-runtime/runtime';
 	import 'core-js/es6/map';
 	import 'core-js/es6/number';
 	import 'core-js/es6/promise';
 	import 'core-js/es6/symbol';
 	import 'core-js/fn/array/from';
 	import 'core-js/modules/es7.symbol.async-iterator';
-	import 'regenerator-runtime/runtime';
 	import 'whatwg-fetch';
 	export * from 'dsteem';
 
